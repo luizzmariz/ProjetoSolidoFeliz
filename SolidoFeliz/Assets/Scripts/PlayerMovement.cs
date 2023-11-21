88339a13a986758a8afc571f763649a205514620
DaviFausto <davi.fausto@hotmail.com>
2023-11-20 21:29:57 -0300

    TESTE1

--- SolidoFeliz/Assets/Scripts/PlayerMovement.cs
+++ SolidoFeliz/Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     public float speed;
-    float gay;
+    public float jumpForce;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if(Input.GetAxisRaw("Horizontal") != 0)
         {
-            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
         }
-        else{
-            rb.velocity = new Vector2(0, 0);
-        }
+       
+       if(Input.GetAxisRaw("Vertical") > 0 && isGrounded){
+        rb.AddForce(Vector2.up * jumpForce);
+       }
     }
 }
